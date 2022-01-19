e4cb1d88ca35c4a622425507ca8751bba2cdee04
SUN Julie <sun.julie0@gmail.com>
2022-01-19 12:58:07 +0100

    ajouts fichiers

---
+++ myTestProject/src/Controller/BonjourController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BonjourController extends AbstractController
+{
+    #[Route('/bonjour', name: 'bonjour')]
+    public function index(): Response
+    {
+        $firstname = 'Julie';
+        return $this->render('bonjour/index.html.twig',[
+   
+            'firstname' => $firstname
+        ]);
+    }
+}
